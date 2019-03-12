8632da3b29fd9c3921612a1cbdff54a1ef1cdae7
cnFreeZz <524322322@qq.com>
2019-03-12 14:35:16 +0800

    0312

---
+++ 20190312/20190312/page/static/js/tj.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "hm.js-f43a8fa1ee78bba13592abff575f662f"/*tpa=https://hm.baidu.com/hm.js?f43a8fa1ee78bba13592abff575f662f*/;
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
