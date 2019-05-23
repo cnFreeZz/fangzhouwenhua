961556e4f022d0d39872ed8bf3d2c002022fd0a7
cnFreeZz <524322322@qq.com>
2019-05-23 18:44:40 +0800

    fxddd

---
+++ fx/image/count.js
@@ -0,0 +1,10 @@
+//TV
+var _hmt = _hmt || [];
+(function() {
+	"use strict";
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?339e76ca32baca69f127cab1cead500e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1277154065'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s96.cnzz.com/z_stat.php%3Fid%3D1277154065%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));
---
+++ fx/image/wxID.js
@@ -0,0 +1,7 @@
+
+	var pageID=34;
+	var weixinhao='xiaoxu88803';
+	var weixinID={
+		weixin:weixinhao,
+		cover:'/erweima/' + weixinhao + '.jpg'
+	};
