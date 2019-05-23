0a96d69e35abbcbf045cc7874db595f953671abe
cnFreeZz <524322322@qq.com>
2019-05-23 15:03:26 +0800

    sdf

---
+++ aa/yys_files/dy.php
@@ -0,0 +1,6 @@
+var Arr =[' 123456'];
+var n = Math.round(Math.random() * (Arr.length - 1));
+
+
+
+
---
+++ aa/yys_files/jf.js
@@ -0,0 +1,9 @@
+var arr_wx =['123456'];
+
+var wx_index = Math.floor((Math.random() * arr_wx.length));
+var wxh = arr_wx[wx_index];
+
+
+
+
+
